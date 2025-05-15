ed6152da441eda432a561b3287c7507cbc924c47
Gaby8973621 <gabriela472151@gmail.com>
2025-05-15 16:02:58 -0600

    Inicializando el proyecto

---
+++ database/migrations/2025_05_15_001816_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->string('urlimagen',50)->nullable();
+            $table->unsignedBigInteger('categoria_id');
+            //relacion con categorias
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+            //nose si hay que poner user
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
